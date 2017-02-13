5ba53347a2642dacc02a65a2f4dddfb9ab2c1f12
Esseh <kennethwilleford@gmail.com>
2017-02-12 21:07:35 -0800

    Refactored NOTES_Handlers.go further. Encapsulated modification behavior as functions.

--- NOTES_Handlers.go
+++ NOTES_Handlers.go
@@ -5,10 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/Esseh/retrievable"
 	"github.com/julienschmidt/httprouter"
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/log"
 )
 
 const (
@@ -38,44 +35,32 @@ func NOTES_POST_New(res http.ResponseWriter, req *http.Request, params httproute
 
 	protected, boolConversionError := strconv.ParseBool(req.FormValue("protection"))
 	if ErrorPage(ctx, res, nil, "Internal Server Error (1)", boolConversionError, http.StatusSeeOther) { return }
+
 	
-	NewContent := Content{
-		Title:   req.FormValue("title"),
-		Content: req.FormValue("note"),
-	}
-
-	key, err := retrievable.PlaceEntity(ctx, int64(0), &NewContent)
+	_, noteKey, err := CreateNewNote(ctx,
+		Content{
+			Title:   req.FormValue("title"),
+			Content: req.FormValue("note"),
+		},
+		Note{
+			OwnerID:   int64(user.IntID),
+			Protected: protected,
+		},
+	)
 	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) { return }
-
-	NewNote := Note{
-		OwnerID:   int64(user.IntID),
-		Protected: protected,
-		ContentID: key.IntID(),
-	}
-
-	newkey, err := retrievable.PlaceEntity(ctx, int64(0), &NewNote)
-	if ErrorPage(ctx, res, nil, "Internal Server Error (3)", err, http.StatusSeeOther) { return }
-	http.Redirect(res, req, "/view/"+strconv.FormatInt(newkey.IntID(), 10), http.StatusSeeOther)
+	
+	http.Redirect(res, req, "/view/"+strconv.FormatInt(noteKey.IntID(), 10), http.StatusSeeOther)
 }
 
 func NOTES_GET_View(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	user, validated := MustLogin(res,req);  if !validated { return }
 	ctx := NewContext(res,req)
 
-	NoteKey, intConversionError := strconv.ParseInt(params.ByName("ID"), 10, 64)
-	if ErrorPage(ctx, res, nil, "Internal Server Error (1)", intConversionError, http.StatusSeeOther) { return }
-
-	ViewNote := &Note{}
-	ViewContent := &Content{}
-
-	err := retrievable.GetEntity(ctx, NoteKey, ViewNote)
-	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) { return }
-
-	err = retrievable.GetEntity(ctx, ViewNote.ContentID, ViewContent)
-	if ErrorPage(ctx, res, nil, "Internal Server Error (3)", err, http.StatusSeeOther) { return }
+	ViewNote, ViewContent, err := GetExistingNote(params.ByName("ID"), ctx)
+	if ErrorPage(ctx, res, nil, "Internal Server Error (1)", err, http.StatusSeeOther) { return }
 
 	owner, err := GetUserFromID(ctx, ViewNote.OwnerID)
-	if ErrorPage(ctx, res, nil, "Internal Server Error (4)", err, http.StatusSeeOther) { return }
+	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) { return }
 
 	NoteBody := template.HTML(EscapeString(ViewContent.Content))
 
@@ -97,21 +82,13 @@ func NOTES_GET_View(res http.ResponseWriter, req *http.Request, params httproute
 
 }
 
-
-
-
-
-
-
-
-
 func NOTES_GET_Editor(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	user, validated := MustLogin(res,req); if !validated { return }
 	ctx := NewContext(res,req)
-	ViewNote, ViewContent, err := GetNoteData(params.ByName("ID"), ctx)
+	ViewNote, ViewContent, err := GetExistingNote(params.ByName("ID"), ctx)
 	if ErrorPage(ctx, res, nil, "Internal Server Error (1)", err, http.StatusSeeOther) { return }
 
-	validated := VerifyNotePermission(res, req, user, ViewNote); if !validated { return }
+	validated = VerifyNotePermission(res, req, user, ViewNote); if !validated { return }
 
 	Body := template.HTML(ViewContent.Content)
 	ServeTemplateWithParams(res, "editnote", struct {
@@ -128,84 +105,22 @@ func NOTES_GET_Editor(res http.ResponseWriter, req *http.Request, params httprou
 	})
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/// TODO: implement
 func NOTES_POST_Editor(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
-	u, err := GetUserFromSession(req) // Check if a user is already logged in.
-	//TODO DETERMInE IF THEY HAVE PERMISSION	// Should check in both GET and POST
-	ctx := appengine.NewContext(req)
-	if err != nil {
-		http.Redirect(res, req, "/"+req.FormValue("redirect"), http.StatusSeeOther)
-		return
-	}
+	user, validated := MustLogin(res,req); if !validated { return }
+	ctx := NewContext(res,req)
 
-	data := req.FormValue("note")
-	title := req.FormValue("title")
-	notekey := req.FormValue("notekey")
-	protection := req.FormValue("protection")
-	log.Infof(ctx, "protections string is :", protection)
-	protbool, err := strconv.ParseBool(protection)
-	if ErrorPage(ctx, res, nil, "Internal Server Error (5)", err, http.StatusSeeOther) {
-		return
-	}
+	protbool, boolConversionError := strconv.ParseBool(req.FormValue("protection"))
+	if ErrorPage(ctx, res, nil, "Internal Server Error (1)", boolConversionError, http.StatusSeeOther) { return }
 
-	Note := &Note{}
-
-	intkey, err := strconv.ParseInt(notekey, 10, 64)
-
-	err = retrievable.GetEntity(ctx, intkey, Note)
-	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) {
-		return
-	}
-
-	// Permission Check, For collaberation it can also check against a collaborator container after the user check.
-	// When setting for example, privacy setting might only be able to be set by the Owner so a separation is still needed.
-	if Note.OwnerID != int64(u.IntID) && Note.Protected {
-		// Soft rejection. can also be substituted for a http Not Allowed.
-		http.Redirect(res, req, "/view/"+notekey, http.StatusSeeOther)
-		return
-	}
-
-	Content := &Content{}
-
-	err = retrievable.GetEntity(ctx, Note.ContentID, Content)
-	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) {
-		return
-	}
-
-	tempcontent := EscapeString(data)
-
-	Content.Content = tempcontent
-	Content.Title = title
-	if Note.OwnerID == int64(u.IntID) {
-		Note.Protected = protbool
-	}
-
-	_, err = retrievable.PlaceEntity(ctx, intkey, Note)
-	if ErrorPage(ctx, res, nil, "Internal Server Error (3)", err, http.StatusSeeOther) {
-		return
-	}
-
-	_, err = retrievable.PlaceEntity(ctx, Note.ContentID, Content)
-	if ErrorPage(ctx, res, nil, "Internal Server Error (4)", err, http.StatusSeeOther) {
-		return
-	}
-
-	http.Redirect(res, req, "/view/"+notekey, http.StatusSeeOther)
+	err := UpdateNoteContent(ctx, res,req ,user ,req.FormValue("notekey"),
+		Content{
+			Content: EscapeString(req.FormValue("note")),
+			Title: req.FormValue("title"),
+		},
+		Note{
+			Protected: protbool,
+		},
+	)
+	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) { return }	
+	http.Redirect(res, req, "/view/"+req.FormValue("notekey"), http.StatusSeeOther)
 }
--- UNPLACED_FUNCTIONS.go
+++ UNPLACED_FUNCTIONS.go
@@ -1,10 +1,12 @@
 package main
 import (
+	"errors"
 	"net/http"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 	"strconv"
 	"github.com/Esseh/retrievable"
+	"google.golang.org/appengine/datastore"
 )
 
 
@@ -16,7 +18,7 @@ func NewContext(res http.ResponseWriter, req *http.Request) Context{
 	return Context { appengine.NewContext(req) }
 }
 
-func GetNoteData(id string, ctx context.Context)(*Note,*Content,error){
+func GetExistingNote(id string, ctx context.Context)(*Note,*Content,error){
 	RetrievedNote := &Note{}
 	RetrievedContent := &Content{}
 	NoteKey, err := strconv.ParseInt(id, 10, 64)
@@ -28,9 +30,28 @@ func GetNoteData(id string, ctx context.Context)(*Note,*Content,error){
 }
 
 func VerifyNotePermission(res http.ResponseWriter, req *http.Request, user *User, note *Note) bool {
+	redirect := strconv.FormatInt(note.OwnerID, 10)
 	if note.OwnerID != int64(user.IntID) && note.Protected {
-		http.Redirect(res, req, "/view/"+params.ByName("ID"), http.StatusSeeOther)
+		http.Redirect(res, req, "/view/"+redirect, http.StatusSeeOther)
 		return false
 	}
 	return true
+}
+
+func CreateNewNote(ctx context.Context,NewContent Content,NewNote Note) (*datastore.Key,*datastore.Key,error) {
+	contentKey, err := retrievable.PlaceEntity(ctx, int64(0), &NewContent)
+	if err != nil { return contentKey,&datastore.Key{},err }
+	NewNote.ContentID = contentKey.IntID()
+	noteKey, err := retrievable.PlaceEntity(ctx, int64(0), &NewNote)
+	return contentKey,noteKey,err
+}
+
+func UpdateNoteContent(ctx context.Context, res http.ResponseWriter,req *http.Request,user *User,id string,UpdatedContent Content, UpdatedNote Note) error {
+	Note := Note{}
+	noteID, err := strconv.ParseInt(id, 10, 64); if err != nil { return err }
+	err = retrievable.GetEntity(ctx, noteID, &Note); if err != nil { return err }
+	validated := VerifyNotePermission(res, req, user, &Note); if !validated { return errors.New("Permission Error: Not Allowed") }
+	if Note.OwnerID == int64(user.IntID) { Note.Protected = UpdatedNote.Protected }	
+	_, err = retrievable.PlaceEntity(ctx, noteID, &Note); if err != nil { return err }
+	_, err = retrievable.PlaceEntity(ctx, Note.ContentID, &UpdatedContent); return err
 }
