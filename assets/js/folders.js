91373968f3e5d7a28f4ba1e48d67c6afc70a98b7
matthewdavies27 <matthewdavies27@gmail.com>
2017-03-13 20:32:35 -0700

    Folders fully functional, no error reports yet.

--- assets/js/folders.js
+++ assets/js/folders.js
@@ -22,13 +22,10 @@ var clickFolder = function (event) {
   // Close and empty all open folders that aren't in the parent chain.
   let parentId = document.getElementById(clickedDiv).getAttribute('value');
   let folderDivs = (document.getElementsByClassName("open-folder"));
-  console.log(folderDivs);
     for (let folderDiv of folderDivs) {
       let folderID = folderDiv.getAttribute('id');
       if(!(('' + clickedDiv).indexOf('' + folderID + '/') != -1)) {
         if ((document.getElementById('' + folderID + '-content').innerHTML) != "") {
-          console.log("trying to close a div");
-          console.log(folderID);
           document.getElementById('' + folderID + '-content').innerHTML = "";
           $(document.getElementById('' + folderID + '-content')).removeClass("open-content");
           $(document.getElementById('' + folderID + '-content')).addClass("content");
@@ -63,7 +60,7 @@ var refreshContent = function (folderId) {
 // This will close the folder, all menus, and any folders inside of this one, the open the menu of it's parent folder.
 var clickOpenFolder = function (event) {
   // Close and empty folder content.
-  var clickedDiv = event.target.id;
+  let clickedDiv = event.target.id;
   document.getElementById('' + clickedDiv + '-content').innerHTML = "";
   $(document.getElementById('' + clickedDiv + '-content')).unbind();
   $(document.getElementById('' + clickedDiv + '-content')).removeClass("open-content");
@@ -78,10 +75,7 @@ var clickOpenFolder = function (event) {
   // Empty and close all menus.
   let menuDivs = (document.getElementsByClassName("open-menu"));
     for (let menuDiv of menuDivs) {
-      console.log(menuDiv);
-      console.log((menuDiv).innerHTML);
       menuDiv.innerHTML = "";
-      console.log(document.getElementById(menuDiv.id.innerHTML));
       $(menuDiv).removeClass('open-menu');
       $(menuDiv).addClass('menu');
     }
@@ -91,7 +85,7 @@ var clickOpenFolder = function (event) {
   let folderDivs = (document.getElementsByClassName("open-folder"));
     for (let folderDiv of folderDivs) {
       let folderID = folderDiv.getAttribute('id');
-      if(!(('' + folderID).indexOf('' + parentId + '/') != -1)) {
+      if(!(('' + clickedDiv).indexOf('' + folderID + '/') != -1)) {
         if ((document.getElementById('' + folderID + '-content').innerHTML) != "") {
           document.getElementById('' + folderID + '-content').innerHTML = "";
           $(document.getElementById('' + folderID + '-content')).removeClass("open-content");
@@ -103,13 +97,11 @@ var clickOpenFolder = function (event) {
         }
       }
     }
-
-  if(document.getElementById(clickedDiv).getAttribute('value') != "root") {
+  if((document.getElementById('' + clickedDiv)).getAttribute('value') != "root") {
 
     //If the folder is not the root folder, then open the parent folder's menu and add content.
     $(document.getElementById("" + parentId + '-menu')).append(
         '<button id="' + parentId + '-remove-folder" class="remove-folder" value="' + parentId + '"> Delete Folder </button>' +
-        '<input id="' + parentId + '-user-input" class="user-input" type=text value="" placeholder=""/>' +
         '<button id="' + parentId + '-add-folder" class="add-folder" value="' + parentId + '"> New Folder </button>' +
         '<button id="' + parentId + '-add-note" class="add-note" value="' + parentId + '"> Add Note </button>');
     $(document.getElementById('' + parentId + '-menu')).removeClass('menu');
@@ -124,28 +116,39 @@ var clickOpenFolder = function (event) {
 };
 
 // This is the function called when Add Folder button is clicked. T
-// This will take the click event info and feed it into the addFolder function.
+// This will create a prompt for an input value, and feed the input value and event data into the addFolder function.
 var clickAddFolder = function (event) {
-  let baseFolder = event.target.value;
-  let input = document.getElementById("" + baseFolder + "-user-input");
-  addFolder(baseFolder, input.value);
+  let confirmFolderName = function() {
+    let inputValue = document.getElementById('prompt-input').value;
+    let baseFolder = event.target.value;
+    addFolder(baseFolder, inputValue);
+  }
+  createPrompt('Enter the name of the folder you would like to create.', confirmFolderName);
 }
 
 // This is the function called when Remove Folder button is clicked.
 // This will take the click event info and feed it into the removeFolder function.
 var clickRemoveFolder = function (event) {
+  let confirmFolderName = function() {
+  console.log(document.getElementById('prompt-input'));
+  let inputValue = document.getElementById('prompt-input').value;
+  console.log(document.getElementById('prompt-input').value);
   let baseFolder = event.target.value;
-  let input = document.getElementById("" + baseFolder + "-user-input");
-  removeFolder(baseFolder, input.value);
+  removeFolder(baseFolder, inputValue);
+  }
+  createPrompt('Enter the name of the folder you would like to delete.', confirmFolderName);
 }
 
 // This is the functioned called when Add Note button is clicked.
 // This will take the click event info and feed it into the addNote function.
 var clickAddNote = function (event) {
+  let confirmNoteURL = function() {
+  let inputValue = document.getElementById('prompt-input').value;
+  console.log(document.getElementById('prompt-input').value);
   let baseFolder = event.target.value;
-  let input = document.getElementById("" + baseFolder + "-user-input");
-  inputValueInt = parseInt(input.value, 10);
-  removeFolder(baseFolder, input.value);
+  addNote(baseFolder, inputValue);
+  }
+  createPrompt('Copy and paste the URL of the note you would like to add.', confirmNoteURL);
 }
 
 // This is the function to open a folder using an api call.
@@ -159,7 +162,6 @@ var openFolder = function (folderID) {
     // Set all open-menus to menus and clear them.
     let menus = document.getElementsByClassName('open-menu');
       for (let menuDiv of menus) {
-        console.log(menuDiv);
         menuDiv.innerHTML = "";
         $(menuDiv).removeClass('open-menu');
         $(menuDiv).addClass('menu');
@@ -167,7 +169,6 @@ var openFolder = function (folderID) {
     // Change the current folders menu to open and add its content.
     $(document.getElementById("" + folderID + '-menu')).append(
         '<button id="' + folderID + '-remove-folder" class="remove-folder" value="' + folderID + '"> Delete Folder </button>' +
-        '<input id="' + folderID + '-user-input" class="user-input" type=text value="" placeholder=""/>' +
         '<button id="' + folderID + '-add-folder" class="add-folder" value="' + folderID + '"> New Folder </button>' +
         '<button id="' + folderID + '-add-note" class="add-note" value="' + folderID + '"> Add Note </button>');
     $(document.getElementById("" + folderID + '-menu')).removeClass('menu');
@@ -192,13 +193,14 @@ var openFolder = function (folderID) {
       }
     }
 
-    //Append all note divs into the folder content
-    for (let noteName of dataObj.notes) {
-      let noteId = "" + folderID + "/" + noteName;
+    //Append all child note divs into the folder content
+    console.log(dataObj.notes);
+    for (let note of dataObj.notes) {
+      let noteId = "" + folderID + "/" + note.id;
       $(document.getElementById("" + folderID + '-content')).append(
-          '<div class="note-container">' +
+          '<div id="' + noteId + '-container" class="note-container">' +
             '<div id="' + noteId + '-remove-note" class="remove-note" value="' + folderID + '"> X </div>' +
-            '<div id="' + noteId + '" class="note"> ' + noteName + '</div>' +
+            '<div id="' + noteId + '" class="note" value="' + note.id + '""> ' + note.title + '</div>' +
           '</div>');
       $(document.getElementById(noteId + "-remove-note")).unbind();
       $(document.getElementById(noteId + "-remove-note")).click(clickRemoveNote);
@@ -219,6 +221,7 @@ var openFolder = function (folderID) {
 var addFolder = function (parentId, folderName) {
   let parentIdString = "" + parentId;
   $.post('/folder/api/newfolder', { ParentID: parentIdString, FolderName: folderName }, function (data) {
+    console.log(data);
     refreshContent(parentId);
   });
 };
@@ -234,19 +237,23 @@ var removeFolder = function (parentId, folderName) {
 // This funcion makes an api call to add a note with the given parameters.
 var addNote = function (parentId, noteId) {
   let parentIdString = "" + parentId;
-  let noteIdInt = parseInt(noteID, 10);
-  $.post('/folder/api/addnote', { ParentID: parentIdString, NoteID: noteIdInt }, function (data) {
+  let noteIdInt = parseInt(noteId.substring(noteId.lastIndexOf('/') + 1), 10);
+  $.post('/folder/api/addnote', { FolderID: parentIdString, NoteID: noteIdInt }, function (data) {
     console.log(data);
     refreshContent(parentId);
   });
 }
 
 // This funcion makes an api call to remove a folder with the given parameters.
-var removeNote = function (parentId, noteId) {
+var removeNote = function (parentId, folderNoteId) {
   let parentIdString = "" + parentId;
-  let noteIdInt = parseInt(noteID, 10);
-  $.post('/folder/api/removenote', { ParentID: parentIdString, NoteID: noteIdInt }, function (data) {
+  let noteId = folderNoteId.substring(folderNoteId.lastIndexOf('/') + 1, (folderNoteId.indexOf('-')));
+  let noteIdInt = parseInt(noteId, 10);
+  console.log(parentIdString);
+  console.log(noteIdInt);
+  $.post('/folder/api/removenote', { FolderID: parentIdString, NoteID: noteIdInt }, function (data) {
     console.log(data);
+    refreshContent(parentId);
   });
 }
 
@@ -257,15 +264,20 @@ var initializeRoot = function (rootId) {
 };
 
 // This is the function called when the remove note button is clicked. It will feed the event info to the removeNote function.
-var clickRemoveNote = function () {
-  console.log('remove note called');
-}
+var clickRemoveNote = function (event) {
+  let baseFolder = event.target.getAttribute('value');
+  let folderNoteId = "" + event.target.id;
+  console.log(baseFolder);
+  console.log(folderNoteId);
+  removeNote(baseFolder, folderNoteId);
+};
 
 // This is the function called when the add note button is clicked. It will feed the event info to the addNote function.
-var openNote = function () {
-  console.log('open Note called');
+var openNote = function (event) {
+  console.log(event.target);
+  window.open('/view/' + event.target.getAttribute("value"), "_blank");
   // will eventually navigate to note.
-}
+};
 
 /////////////////////////////////////
 
@@ -278,5 +290,23 @@ for (let root of rootArray) {
   $(document.getElementById(root.id)).unbind();
   $(document.getElementById(root.id)).click(clickOpenFolder);
 }
+
+////////////////////////////////////
+
+var closePrompt = function() {
+    document.getElementById("prompt-box").style.visibility = "hidden";
+};
+
+var createPrompt = function(message, confirmFunction) {
+    document.getElementById('prompt-box').style.visibility = "visible";
+    document.getElementById('prompt-box').innerHTML = '<div id="prompt-close">x</div>' +
+        '<div id="prompt-message">' + message + '</div>' +
+        '<input id="prompt-input" type="text" name="prompt-input" value=""/>' +
+        '<button id="prompt-confirm"> Confirm </button>';
+    $(document.getElementById('prompt-confirm')).unbind();
+    $(document.getElementById('prompt-confirm')).click(confirmFunction);
+    $(document.getElementById('prompt-confirm')).click(closePrompt);
+    $(document.getElementById('prompt-close')).click(closePrompt);
+};
 
 }
