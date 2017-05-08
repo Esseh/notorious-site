4e287eec136cd3516213f76c92585e892c198d06
matthewdavies27 <matthewdavies27@gmail.com>
2017-05-07 20:33:14 -0700

    Notifications Working

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -3,6 +3,15 @@ function openDrop() {
     document.getElementById("dropeddown").classList.toggle("show");
 }
 
+function clearNotif() {
+  $.post("/NOTIFICATION/api/clear", function( data ) {
+  });
+    document.getElementById("dropeddown").classList.toggle("show");
+    document.getElementById("newbutton").innerHTML = "No New Notifications";
+    document.getElementById("newbutton").removeAttribute("onclick");
+    document.getElementById("newbutton").style.backgroundColor = "gray";
+}
+
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
 
