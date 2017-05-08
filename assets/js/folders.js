174148b3ed35f4434a0ffc706fde3767acf2899a
matthewdavies27 <matthewdavies27@gmail.com>
2017-05-08 16:12:39 -0700

    Some CSS fixes
    Changed CSS on notifications and folders.

--- assets/js/folders.js
+++ assets/js/folders.js
@@ -189,6 +189,7 @@ var clickRemoveNote = function (event) {
 
 var clickCopyNote = function (event) {
   let noteId = event.target.getAttribute('value');
+  event.target.style.backgroundImage = "url(/assets/images/CheckmarkIcon.png)"
   copyNote(noteId);
 }
 
@@ -248,7 +249,7 @@ var openFolder = function (folderID) {
         $(document.getElementById("" + folderID + '-content')).append(
             '<div id="' + noteId + '-container" class="note-container">' +
               '<div id="' + noteId + '-remove-note" class="remove-note" value="' + folderID + '"> X </div>' +
-              '<div id="' + noteId + '-copy-note" class="copy-note" value="' + note.id + '"> Copy </div>' +
+              '<div id="' + noteId + '-copy-note" class="copy-note" value="' + note.id + '"> </div>' +
               '<div id="' + noteId + '" class="note" value="' + note.id + '""> ' + note.title + '</div>' +
             '</div>');
         $(document.getElementById(noteId + "-remove-note")).unbind();
